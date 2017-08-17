296b7467a9a208a97e79c93f0173add3f2cd80ee
krishnamoorthy <krishnamoorthy@gpslsolutions.com>
2017-08-17 21:06:42 +0530

    Login module completed TODO# 0. facebook authentication using passport 1. show all reviews from database 2. create new reviews 3. adding comments to reviews 4. get upvotes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,17 @@
-var util = require('util');
 var express = require('express');
+var crypto = require('crypto');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 //couch db connection and database call
 var db = require('nano')('http://localhost:5984/bookreview');
+
+//sub function hash to encrypt the entered password
+function hash(input,salt){
+  var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+  return ["pbkdf2","10000", salt, hashed.toString('hex')].join('$');
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,11 +19,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/signin', function(req, res, next){
-  res.render('signin', { title : 'Login'});
+  res.render('signin', { title : 'Signin'});
 });
 
 router.get('/signup', function(req, res, next){
-  res.render('signup', { title : 'Sign up'});
+  res.render('signup', { title : 'Signup'});
 });
 
 router.post('/signup', function(req, res, next){
@@ -30,6 +39,10 @@ router.post('/signup', function(req, res, next){
   req.checkBody('password','Password should not be empty').notEmpty();
   req.checkBody('password2','Password should match').equals(req.body.password);
 
+  //hash the submitted password
+  var salt = crypto.randomBytes(128).toString('hex');
+  password = hash(password,salt);
+  //then use the hashed password to store in the database
   //Get the validation result
   req.getValidationResult().then(function(result){
       if (! result.isEmpty() ) {
@@ -56,6 +69,53 @@ router.post('/signup', function(req, res, next){
         res.redirect('/');
       }
   });
+});
+
+passport.serializeUser(function(body, done){
+  done(null,body.rows[0].id);
+});
+
+passport.deserializeUser(function(id, done){
+  db.get('_design/getUser/_view/view-all?key="' + id + '"',{revs_info:true},function(err,body){
+    done(err,body);
+  });
+});
+//DB authentication done here using passport
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    db.get('_design/getUser/_view/user-view?key="' + username + '"', {revs_info:true} ,function(err,body){
+      if (err) console.log(err);
+      if (! body){
+        console.log('Unknown user');
+        return done(null, false, {message : 'Unknown User'});
+      }
+      //console.log(body.rows[0].value);
+      var dbpassword = body.rows[0].value;
+      var salt = dbpassword.split('$')[2];
+      var hashedpassword = hash(password,salt);
+      var isMatch = false;
+      if (dbpassword === hashedpassword) isMatch = true;
+      if (isMatch) {
+        return done(null, body);
+      } else {
+        console.log('Invalid password');
+        return done(null, false, {message : 'Invalid Password'});
+      }
+    });
+  }
+));
+
+//Login post method
+router.post('/signin', passport.authenticate('local',{failureRedirect:'/users/login',failureFlash:'Login Authentication Failed'}), function(req, res, next){
+  console.log('Authentication successfull');
+  req.flash('success','Login Successfull');
+  res.redirect('/');
+});
+
+router.get('/logout', function(req, res, next){
+  req.logout();
+  req.flash('success','User Logged out');
+  res.redirect('/users/signin');
 });
 
 module.exports = router;
